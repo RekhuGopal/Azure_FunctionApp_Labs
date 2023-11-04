08e14603996eb7c0e10a01f9213c60fd980d56ac
RekhuGopal <vrchinnarathod@gmail.com>
2023-11-04 16:07:26 +0530

    Updated

--- FunctionApp/main.tf
+++ FunctionApp/main.tf
@@ -68,3 +68,31 @@ resource "azurerm_function_app" "funcdeploy" {
     type = "SystemAssigned"
    }
 }
+
+resource "azurerm_function_app_function" "example" {
+  name            = "example-function-app-function"
+  function_app_id = azurerm_function_app.funcdeploy.id
+  language        = "Python"
+  test_data = jsonencode({
+    "name" = "Azure"
+  })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
